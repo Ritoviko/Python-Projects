a2ea9c9f5699d8016bda7867000fbe3018510f4c
Ritoviko <95902980+Ritoviko@users.noreply.github.com>
2022-09-16 12:50:33 +0530

    Add files via upload

---
+++ zodiac signs.py
@@ -0,0 +1,346 @@
+print("Enter your birth date: ")
+while True:
+    month = input("Month (in short like Jan): ")
+    day= int(input("Day: "))
+
+    if month == "jan" or month == "Jan":
+        if day <20:
+            print("Your zodiac sign is: CAPRICORN")
+
+            opt = input("Do you want to check more dates? (Y/N): ")
+            if opt == "Y"or opt == "y":
+                print("ok")
+                print("--------------------------------------------------------")
+            elif opt == "N" or opt == "n":
+                print("Bye - Bye")
+                print("--------------------------------------------------------")
+                break
+            else:
+                print("Wrong input")
+        elif day>=20 and day<=31:
+            print("Your zodiac sign is: AQUARIUS")
+            opt = input("Do you want to check more dates? (Y/N): ")
+            if opt == "Y"or opt == "y":
+                print("ok")
+                print("--------------------------------------------------------")
+            elif opt == "N" or opt == "n":
+                print("Bye - Bye")
+                print("--------------------------------------------------------")
+                break
+            else:
+                print("Wrong input")
+        else:
+            print("Wrong day input.")
+            
+    elif month == "feb" or month == "Feb":
+        if day <19:
+            print("Your zodiac sign is: AQUARIUS")
+            opt = input("Do you want to check more dates? (Y/N): ")
+            if opt == "Y"or opt == "y":
+                print("ok")
+                print("--------------------------------------------------------")
+            elif opt == "N" or opt == "n":
+                print("Bye - Bye")
+                print("--------------------------------------------------------")
+                break
+            else:
+                print("Wrong input")
+        elif day>=19 and day<=28:
+            print("Your zodiac sign is: PISCES")
+            opt = input("Do you want to check more dates? (Y/N): ")
+            if opt == "Y"or opt == "y":
+                print("ok")
+                print("--------------------------------------------------------")
+            elif opt == "N" or opt == "n":
+                print("Bye - Bye")
+                print("--------------------------------------------------------")
+                break
+            else:
+                print("Wrong input")
+        else:
+            print("Wrong day input.")
+        
+    elif month == "mar" or month == "Mar":
+        if day <21:
+            print("Your zodiac sign is: PISCES")
+            opt = input("Do you want to check more dates? (Y/N): ")
+            if opt == "Y"or opt == "y":
+                print("ok")
+                print("--------------------------------------------------------")
+            elif opt == "N" or opt == "n":
+                print("Bye - Bye")
+                print("--------------------------------------------------------")
+                break
+            else:
+                print("Wrong input")
+        elif day>=21 and day<=31:
+            print("Your zodiac sign is: ARIES")
+            opt = input("Do you want to check more dates? (Y/N): ")
+            if opt == "Y"or opt == "y":
+                print("ok")
+                print("--------------------------------------------------------")
+            elif opt == "N" or opt == "n":
+                print("Bye - Bye")
+                print("--------------------------------------------------------")
+                break
+            else:
+                print("Wrong input")
+        else:
+            print("Wrong day input.")
+            
+    elif month == "Apr" or month == "apr":
+        if day <20:
+            print("Your zodiac sign is: ARIES")
+            opt = input("Do you want to check more dates? (Y/N): ")
+            if opt == "Y"or opt == "y":
+                print("ok")
+                print("--------------------------------------------------------")
+            elif opt == "N" or opt == "n":
+                print("Bye - Bye")
+                print("--------------------------------------------------------")
+                break
+            else:
+                print("Wrong input")
+        elif day>=20 and day<=30:
+            print("Your zodiac sign is: TAURUS")
+            opt = input("Do you want to check more dates? (Y/N): ")
+            if opt == "Y"or opt == "y":
+                print("ok")
+                print("--------------------------------------------------------")
+            elif opt == "N" or opt == "n":
+                print("Bye - Bye")
+                print("--------------------------------------------------------")
+                break
+            else:
+                print("Wrong input")
+        else:
+            print("Wrong day input.")
+            
+    elif month == "May" or month == "may":
+        if day <21:
+            print("Your zodiac sign is: TAURUS")
+            opt = input("Do you want to check more dates? (Y/N): ")
+            if opt == "Y"or opt == "y":
+                print("ok")
+                print("--------------------------------------------------------")
+            elif opt == "N" or opt == "n":
+                print("Bye - Bye")
+                print("--------------------------------------------------------")
+                break
+            else:
+                print("Wrong input")
+        elif day>=21 and day<=31:
+            print("Your zodiac sign is: GEMINI")
+            opt = input("Do you want to check more dates? (Y/N): ")
+            if opt == "Y"or opt == "y":
+                print("ok")
+                print("--------------------------------------------------------")
+            elif opt == "N" or opt == "n":
+                print("Bye - Bye")
+                print("--------------------------------------------------------")
+                break
+            else:
+                print("Wrong input")
+        else:
+            print("Wrong day input.")
+            
+    elif month == "june" or month == "June":
+        if day <21:
+            print("Your zodiac sign is: GEMINI")
+            opt = input("Do you want to check more dates? (Y/N): ")
+            if opt == "Y"or opt == "y":
+                print("ok")
+                print("--------------------------------------------------------")
+            elif opt == "N" or opt == "n":
+                print("Bye - Bye")
+                print("--------------------------------------------------------")
+                break
+            else:
+                print("Wrong input")
+        elif day>=21 and day<=30:
+            print("Your zodiac sign is: CANCER")
+            opt = input("Do you want to check more dates? (Y/N): ")
+            if opt == "Y"or opt == "y":
+                print("ok")
+                print("--------------------------------------------------------")
+            elif opt == "N" or opt == "n":
+                print("Bye - Bye")
+                print("--------------------------------------------------------")
+                break
+            else:
+                print("Wrong input")
+        else:
+            print("Wrong day input.")
+            
+    elif month == "july" or month == "July":
+        if day <23:
+            print("Your zodiac sign is: CANCER")
+            opt = input("Do you want to check more dates? (Y/N): ")
+            if opt == "Y"or opt == "y":
+                print("ok")
+                print("--------------------------------------------------------")
+            elif opt == "N" or opt == "n":
+                print("Bye - Bye")
+                print("--------------------------------------------------------")
+                break
+            else:
+                print("Wrong input")
+        elif day>=23 and day<=31:
+            print("Your zodiac sign is: LEO")
+            opt = input("Do you want to check more dates? (Y/N): ")
+            if opt == "Y"or opt == "y":
+                print("ok")
+                print("--------------------------------------------------------")
+            elif opt == "N" or opt == "n":
+                print("Bye - Bye")
+                print("--------------------------------------------------------")
+                break
+            else:
+                print("Wrong input")
+        else:
+            print("Wrong day input.")
+    
+    elif month == "Aug" or month == "aug":
+        if day <23:
+            print("Your zodiac sign is: LEO")
+            opt = input("Do you want to check more dates? (Y/N): ")
+            if opt == "Y"or opt == "y":
+                print("ok")
+                print("--------------------------------------------------------")
+            elif opt == "N" or opt == "n":
+                print("Bye - Bye")
+                print("--------------------------------------------------------")
+                break
+            else:
+                print("Wrong input")
+        elif day>=23 and day<=31:
+            print("Your zodiac sign is: VIRGO")
+            opt = input("Do you want to check more dates? (Y/N): ")
+            if opt == "Y"or opt == "y":
+                print("ok")
+                print("--------------------------------------------------------")
+            elif opt == "N" or opt == "n":
+                print("Bye - Bye")
+                print("--------------------------------------------------------")
+                break
+            else:
+                print("Wrong input")
+        else:
+            print("Wrong day input.")
+    
+    elif month == "Sep" or month == "sep":
+        if day <23:
+            print("Your zodiac sign is: VIRGO")
+            opt = input("Do you want to check more dates? (Y/N): ")
+            if opt == "Y"or opt == "y":
+                print("ok")
+                print("--------------------------------------------------------")
+            elif opt == "N" or opt == "n":
+                print("Bye - Bye")
+                print("--------------------------------------------------------")
+                break
+            else:
+                print("Wrong input")
+        elif day>=23 and day<=30:
+            print("Your zodiac sign is: LIBRA")
+            opt = input("Do you want to check more dates? (Y/N): ")
+            if opt == "Y"or opt == "y":
+                print("ok")
+                print("--------------------------------------------------------")
+            elif opt == "N" or opt == "n":
+                print("Bye - Bye")
+                print("--------------------------------------------------------")
+                break
+            else:
+                print("Wrong input")
+        else:
+            print("Wrong day input.")
+    
+    elif month == "oct" or month == "Oct":
+        if day <23:
+            print("Your zodiac sign is: LIBRA")
+            opt = input("Do you want to check more dates? (Y/N): ")
+            if opt == "Y"or opt == "y":
+                print("ok")
+                print("--------------------------------------------------------")
+            elif opt == "N" or opt == "n":
+                print("Bye - Bye")
+                print("--------------------------------------------------------")
+                break
+            else:
+                print("Wrong input")
+        elif day>=23 and day<=31:
+            print("Your zodiac sign is: SCORPIO")
+            opt = input("Do you want to check more dates? (Y/N): ")
+            if opt == "Y"or opt == "y":
+                print("ok")
+                print("--------------------------------------------------------")
+            elif opt == "N" or opt == "n":
+                print("Bye - Bye")
+                print("--------------------------------------------------------")
+                break
+            else:
+                print("Wrong input")
+        else:
+            print("Wrong day input.")
+    
+    elif month == "nov" or month == "Nov":
+        if day <22:
+            print("Your zodiac sign is: SCORPIO")
+            opt = input("Do you want to check more dates? (Y/N): ")
+            if opt == "Y"or opt == "y":
+                print("ok")
+                print("--------------------------------------------------------")
+            elif opt == "N" or opt == "n":
+                print("Bye - Bye")
+                print("--------------------------------------------------------")
+                break
+            else:
+                print("Wrong input")
+        elif day>=22 and day<=30:
+            print("Your zodiac sign is: SAGITTARIUS")
+            opt = input("Do you want to check more dates? (Y/N): ")
+            if opt == "Y"or opt == "y":
+                print("ok")
+                print("--------------------------------------------------------")
+            elif opt == "N" or opt == "n":
+                print("Bye - Bye")
+                print("--------------------------------------------------------")
+                break
+            else:
+                print("Wrong input")
+        else:
+            print("Wrong day input")
+    
+    elif month == "dec" or month == "Dec":
+        if day <22:
+            print("Your zodiac sign is: SAGITTARIUS")
+            opt = input("Do you want to check more dates? (Y/N): ")
+            if opt == "Y"or opt == "y":
+                print("ok")
+                print("--------------------------------------------------------")
+            elif opt == "N" or opt == "n":
+                print("Bye - Bye")
+                print("--------------------------------------------------------")
+                break
+            else:
+                print("Wrong input")
+        elif day>=22 and day<=31:
+            print("Your zodiac sign is: CAPRICORN")
+            opt = input("Do you want to check more dates? (Y/N): ")
+            if opt == "Y"or opt == "y":
+                print("ok")
+                print("--------------------------------------------------------")
+            elif opt == "N" or opt == "n":
+                print("Bye - Bye")
+                print("--------------------------------------------------------")
+                break
+            else:
+                print("Wrong input")
+        else:
+            print("Wrong day input")
+            
+    
+    else:
+        print("Wrong month input")
+        print("Please try again.")
